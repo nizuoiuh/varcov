243487cd8e5ff73a45621efc43c3961bcc509b43
nizuoiuh <nizuoiuh@users.noreply.github.com>
2025-01-12 18:55:34 +0100

    calculate variance covariance matrix with eigen

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,50 @@
+#include "csv.hpp"
+#include <eigen3/Eigen/Core>
 #include <iostream>
-#include <numeric>
 #include <vector>
 
-float mean(std::vector<double> const &v) {
-  auto const count = static_cast<float>(v.size());
-  return std::reduce(v.begin(), v.end()) / count;
-}
+using namespace csv;
 
 int main() {
-  std::vector<double> x = {1.f, 2.3f, 3.5f, 5.0f, 2.5f, 4.2f, 3.4f};
-  std::vector<double> y = {.2f, 2.2f, 2.5f, 4.0f, 3.5f, 5.2f, 4.4f};
-  auto x_mean = mean(x);
-  auto y_mean = mean(y);
+  std::cout << "Eigen version: " << EIGEN_MAJOR_VERSION << "."
+            << EIGEN_MINOR_VERSION << std::endl;
 
-  auto size = x.size();
-  auto sum = 0.0f;
-  for (size_t i = 0; i < size; i++) {
-    sum += (x[i] - x_mean) * (y[i] - y_mean);
+  std::vector<std::vector<double>> returns(10, std::vector<double>());
+  int i = 0;
+  CSVReader reader("returns.csv");
+  for (CSVRow &row : reader) { // Input iterator
+    for (CSVField &field : row) {
+      if (i == 0) {
+        i++;
+        continue;
+      } else {
+        returns[i - 1].push_back(field.get<double>());
+        i++;
+      }
+    }
+    i = 0;
   }
-  double cov = sum / (size - 1);
+
+  std::cout << returns[0].size() << std::endl;
+  std::cout << returns.size() << std::endl;
+
+  // i have not found simple construction of eigen matrix from
+  // vector<vector<double>>
+  Eigen::MatrixXd X(returns[0].size(), returns.size());
+  for (size_t n = 0; n < returns.size(); n++) {
+    for (size_t j = 0; j < returns[n].size(); j++) {
+      X(j, n) = returns[n][j];
+    }
+  }
+
+  // std::cout << X << std::endl;
+  Eigen::VectorXd x_mean = X.colwise().mean();
+  Eigen::MatrixXd X_mean = X.rowwise() - x_mean.transpose();
+
+  double k = (1 / (static_cast<double>(X_mean.rows()) - 1));
+
+  Eigen::MatrixXd cov = (X_mean.transpose() * X_mean) * (k);
+
   std::cout << cov << std::endl;
 
   return 0;
